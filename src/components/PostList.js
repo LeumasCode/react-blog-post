76a7e262427df99f59039b8117485d9bbdebd57e
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-17 12:24:49 -0500

    using redux-thunk

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,15 +3,13 @@ import { connect } from "react-redux";
 import { fetchPosts } from "./actions";
 
 class PostList extends Component {
-
-componentDidMount(){
-    this.props.fetchPosts()
-}
-
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
 
   render() {
     return <div>Post List</div>;
   }
 }
 
-export default connect(null, {fetchPosts})(PostList);
+export default connect(null, { fetchPosts })(PostList);
